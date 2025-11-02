fdcaa63e8916917bc31caefa0fd755fa16d5f1aa
Nuria <ncarbonellri@student.unimelb.edu.au>
2025-11-03 00:45:45 +1100

    Fix conflict marker in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
@@ -6,4 +5,3 @@ export default defineConfig({
   base: "/Echos-of-Us-Desktop/", // 👈 must match your GitHub repo name exactly
   plugins: [react()],
 });
->>>>>>> 700a461 (Initial commit)
